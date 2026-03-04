278a9621bf0cd0e2e92aeb4ee1d450da4eb7419c
Ditlev Holt Pedersen <ditlevholtpedersen@gmail.com>
2026-03-04 13:43:55 +0100

    Cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "Følgende vX ændres ved hver relevant ændring - så opdatere alle apps."
 
-const CACHE_NAME = "korttraekker-cache-v4";
+const CACHE_NAME = "korttraekker-cache-v5";
 const ASSETS = [
   "./",
   "./index.html",
