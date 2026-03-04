7c1ce1116ba259420584de4bf855ad53435a70c5
Ditlev Holt Pedersen <ditlevholtpedersen@gmail.com>
2026-03-04 17:22:34 +0100

    Logo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "Følgende vX ændres ved hver relevant ændring - så opdatere alle apps."
 
-const CACHE_NAME = "korttraekker-cache-v6";
+const CACHE_NAME = "korttraekker-cache-v7";
 const ASSETS = [
   "./",
   "./index.html",
